244f2c9448e646581d7a4d0b099b01954f9f48c5
komsunet <146492225+komsunet@users.noreply.github.com>
2023-10-10 16:43:01 +0300

    Update Hero.js

--- themes/shine/components/Hero.js
+++ themes/shine/components/Hero.js
@@ -4,13 +4,10 @@ const Hero = props => {
       <div class='flex max-w-[980px] flex-col items-start gap-2'><h1
         class='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl'>
           <span
-            class='leading-12 block w-full bg-gradient-to-r from-green-400 to-purple-500 bg-clip-text py-2 text-transparent lg:inline'>Üyelik İptali</span>
+            class='leading-12 block w-full bg-gradient-to-r from-green-400 to-purple-500 bg-clip-text py-2 text-transparent lg:inline'>Online İşlemler</span>
       </h1>
         <p class='max-w-[700px] text-lg text-muted-foreground sm:text-xl'>
-          Abonelik iptali, bir hizmet veya ürün için yapılan abonelik anlaşmasının sonlandırılması işlemidir.
-          Abonelikler genellikle belirli bir süre boyunca devam eder ve bu süre sona erdikten sonra otomatik olarak
-          yenilenir. Abonelik iptali, abone olan kişinin veya kurumun bu hizmeti veya ürünü kullanmayı veya ödemeyi
-          durdurmak istediği durumlarda gerçekleşir.
+          Sürekli koşturmaca içindeyseniz ve zamanınız değerliyse, Online İşlemler web sitemiz tam size göre! Sizlere zaman kazandıran, güvenilir ve kullanıcı dostu bir platform sunuyoruz.
         </p>
       </div>
     </div>
