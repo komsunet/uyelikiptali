b9f025f86033105ec71ca9eb2b4c2a86764da23d
komsunet <146492225+komsunet@users.noreply.github.com>
2023-10-10 17:12:46 +0300

    Update Hero.js

--- themes/shine/components/Hero.js
+++ themes/shine/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = props => {
       <div class='flex max-w-[980px] flex-col items-start gap-2'><h1
         class='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl'>
           <span
-            class='leading-12 block w-full bg-gradient-to-r from-green-400 to-purple-500 bg-clip-text py-2 text-transparent lg:inline'>Online İşlemler</span>
+            class='leading-12 block w-full py-2 lg:inline'>Online İşlemler</span>
       </h1>
         <p class='max-w-[700px] text-lg text-muted-foreground sm:text-xl'>
           Sürekli koşturmaca içindeyseniz ve zamanınız değerliyse, Online İşlemler web sitemiz tam size göre! Sizlere zaman kazandıran, güvenilir ve kullanıcı dostu bir platform sunuyoruz.
